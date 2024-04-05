4e8562b305771c75047e2b0efe76dabe1d6060ec
Gabriel Guas <guasgabriel22@gmail.com>
2024-04-05 13:05:50 -0300

    inicio de supertest

---
+++ test/routes/supertest.test.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import supertest from "supertest";
+
+const requester = supertest("http://localhost:8080");
+
+describe("Testing APP", () => {
+  before()
+
+  describe("Testing cart API",() => {
+    
+  })
+})
