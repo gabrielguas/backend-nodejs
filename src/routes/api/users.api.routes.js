434cc37744a4ec74c896794716b3f4fe3d26575f
Gabriel Guas <guasgabriel22@gmail.com>
2024-05-06 13:18:51 -0300

    terminado

--- src/routes/api/users.api.routes.js
+++ src/routes/api/users.api.routes.js
@@ -3,7 +3,7 @@ import userController from "../../controllers/userController.js";
 import hasPermission from "../../middlewares/hasPermissions.middleware.js"
 const router = Router();
 
-router.get("/", userController.getAllUsers);
+router.get("/",hasPermission("admin"), userController.getAllUsers);
 router.delete("/", hasPermission("admin"), userController.deleteUsersTime)
 
 export default router;
